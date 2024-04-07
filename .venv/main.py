5dd9b953773fc7808eb2b466b49e9d385690a723
Eduardo Faustino <eduardofaustino277@gmail.com>
2024-04-07 01:29:34 -0300

    PILOTO

---
+++ .venv/main.py
@@ -0,0 +1,195 @@
+from mysql.connector import connect
+import time
+
+# FUNCOES FUNCOES FUNCOES FUNCOES FUNCOES FUNCOES FUNCOES FUNCOES
+
+def mysql_connection(host, user, passwd, database=None):
+    connection = connect(
+        host=host,
+        user=user,
+        passwd=passwd,
+        database=database
+    )
+    return connection
+
+
+def linha(a=0):
+    print(f"\033[1;{a}m-=\033[m" * 20)
+
+
+def cabeçalho(a="TEXTO"):
+    print(f"\033[1m{a.center(40)}\033[m")
+
+
+def linha2(a=0):
+    print(f"\033[1;{a}m-=\033[m" * 20)
+
+
+def query(comando=""):
+    connection = mysql_connection('localhost', 'root', '', database='testeDB')
+    query = f'''
+                {comando}
+                '''
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.close()
+
+
+def tabela(a=""):
+    connection = mysql_connection('localhost', 'root', '', database='testeDB')
+    query = f'''
+                create table {a.strip()}(
+                `id` varchar(10) primary key not null,
+                `nome` varchar(100) not null,
+                `datacompra` date,
+                `datacheckin` date
+                 );
+                '''
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.close()
+
+
+# FUNCOES FUNCOES FUNCOES FUNCOES FUNCOES FUNCOES FUNCOES FUNCOES
+while True:
+    contador = 0
+
+    linha2(32)
+    cabeçalho("ESCOLHA SEU USUÁRIO:")
+
+    arquivo = open('user.txt', 'rt', encoding='utf8')
+    for linha in arquivo:
+        linha = linha.strip()
+        print(f"[{contador}] - {linha}")
+        contador += 1
+    arquivo.close()
+    contador = 0
+    escolha = int(input("\nDigite o ID do Usuário: "))
+
+    #-----------------------------------------------------------------------------------------
+
+    if escolha == 1:
+        while True:
+            linha2(32)
+            cabeçalho("ADMINISTRADOR")
+            cabeçalho("OPÇÕES: ")
+            print('''[0] - CRIAR NOVO EVENTO
+[1] - VER EVENTOS
+[2] - APAGAR EVENTOS
+[3] - SAIR DO USUÁRIO''')
+            escolhaaction = int(input("\nDigite o Número da Ação: "))
+            linha2(32)
+
+            if escolhaaction == 0:
+                nome = input("Digite o Nome do Evento: ")
+                data = input("Digite a Data do Evento: ")
+                maximo = input("Digite a Capacidade Máxima: ")
+                date = f"{data[4:]}-{data[3:4]}-{data[0:2]}"
+                dato = f"{data[0:2]}/{data[3:4]}/{data[4:]}"
+                name = f"{nome} - {dato} - 0/{maximo}"
+                nume = str(f"{nome}").replace(" ", "_")
+
+                query(comando=f"insert into testeDB.eventos values (default, '{name}', '{date}', '{maximo}', '{nume}')")
+
+                tabela(nome)
+                linha2(34)
+                print("\n\033[32mEVENTO CRIADO COM SUCESSO!\033[m\n")
+                linha2(34)
+                time.sleep(2)
+            if escolhaaction == 1:
+                connection = mysql_connection('localhost', 'root', '', database='testeDB')
+                query = '''
+                    SELECT * FROM eventos;
+                '''
+                cursor = connection.cursor()
+                cursor.execute(query)
+                result = cursor.fetchall()
+                for i, coluna in enumerate(result):
+                    print(f"[{i + 1}] {coluna[1]}\n")
+                connection.close()
+                inspecionar = int(input("Deseja inspecionar qual evento: "))
+
+                connection = mysql_connection('localhost', 'root', '', database='testeDB')
+                query = '''
+                                    SELECT * FROM eventos;
+                                '''
+                cursor = connection.cursor()
+                cursor.execute(query)
+                result = cursor.fetchall()
+                instabela = ""
+                numero = 0
+                for i, v in enumerate(result):
+                    numero = v[0]
+                    if numero == inspecionar:
+                        instabela = v[4]
+                connection.close()
+
+
+
+            if escolhaaction == 2:
+                connection = mysql_connection('localhost', 'root', '', database='testeDB')
+                query = '''
+                                    SELECT * FROM eventos;
+                                '''
+                cursor = connection.cursor()
+                cursor.execute(query)
+                result = cursor.fetchall()
+                for i, coluna in enumerate(result):
+                    print(f"[{i + 1}] {coluna[1]}\n")
+                connection.close()
+                inspecionar = int(input("Deseja Apagar qual evento: "))
+
+                connection = mysql_connection('localhost', 'root', '', database='testeDB')
+                query = '''
+                                    SELECT * FROM eventos;
+                                                '''
+                cursor = connection.cursor()
+                cursor.execute(query)
+                result = cursor.fetchall()
+                aptabela = ""
+                numero = 0
+                for i, v in enumerate(result):
+                    numero = v[0]
+                    if numero == inspecionar:
+                        aptabela = v[4]
+                connection.close()
+
+                #--------------------------------------------------------------------------------------------------
+
+                connection = mysql_connection('localhost', 'root', '', database='testeDB')
+                query = f'''
+                        delete from eventos where (nome = '{aptabela}');
+                                '''
+                cursor = connection.cursor()
+                cursor.execute(query)
+                connection.close()
+
+                #---------------------------------------------------------------------------------------------------
+
+                connection = mysql_connection('localhost', 'root', '', database='testeDB')
+                query = f'''
+                        drop table {aptabela};
+                                                '''
+                cursor = connection.cursor()
+                cursor.execute(query)
+                connection.close()
+
+            if escolhaaction == 3:
+                linha2(31)
+                cabeçalho("SAINDO DO USUÁRIO...")
+                linha2(31)
+                time.sleep(2)
+                break
+
+    if escolha == 2:
+        linha2(33)
+        cabeçalho("REGISTRAR NOVO USUARIO:")
+        nome = input("Digite Seu Nome: ")
+        data = input("Digite Sua Data de Nascimento: ")
+        date = f"{data[4:]}-{data[3:4]}-{data[0:2]}"
+        query(f"insert into users (id, nome, nascimento, idade, evento) values (default, '{nome}','{date}','','')")
+        arquivooo = open('user.txt', 'a', encoding='utf8')
+        arquivooo.write(f"\n{nome.title()}")
+        arquivooo.close()
+
+    #---------------------------------------------------------------------------------------------------------------------------
